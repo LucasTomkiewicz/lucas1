5154d351f69ee95c4a28bfbbf66bbe51208217f7
student9 <student9@pcoip.ki.agh.edu.pl>
2019-06-08 14:56:49 +0200

    Cos

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,22 @@
+package calculator;
+
+public class Calculator {
+
+	public int add(int x, int y) {
+		// TODO Auto-generated method stub
+		return x+y;
+	}
+
+	public Object subtract(int x, int y) {
+		// TODO Auto-generated method stub
+		return x-y;
+	}
+
+	public Object Division(int x, int y) {
+		// TODO Auto-generated method stub
+		return x/y;
+	}
+	
+	
+
+}
---
+++ src/test/java/calculator/CalculatorTest.java
@@ -0,0 +1,37 @@
+package calculator;
+
+import org.junit.Test;
+import org.junit.Before;
+import static org.junit.Assert.*;
+
+public class CalculatorTest {
+	
+	Calculator calc;
+	
+	@Before
+	public void setUp() {
+		calc = new Calculator();
+	}
+	
+	@Test
+	public void testAdd() {
+		//Calculator calc = new Calculator();
+		assertEquals(5, calc.add(3, 2));
+		
+	}
+	
+	@Test
+	public void testSubtract() {
+		//Calculator calc = new Calculator();
+		assertEquals(3, calc.subtract(5, 2));
+		
+	}
+	
+	@Test
+	public void testDivision() {
+		//Calculator calc = new Calculator();
+		assertEquals(3, calc.Division(9, 3));
+		
+	}
+
+}
